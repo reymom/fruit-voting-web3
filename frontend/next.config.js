676d86e927f73ee567cc495658bf9db389b86ca1
reymom <ramon_gs95@hotmail.com>
2023-10-26 21:59:33 +0200

    frontend extra config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push("bufferutil", "utf-8-validate");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
